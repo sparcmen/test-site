e16388d1d7b52082fd129dbda88581944ee11c50
Sparcmen JS <sparcmenjs@gmail.com>
2018-12-26 20:47:13 +0200

    first commit

---
+++ nclude-dependencies/bin/sass-convert.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\Ruby23-x64\bin\ruby.exe" "F:/### Development (Season2)/### My Projects/Personal Blog (jekyll)/hydejack-master/nclude-dependencies/bin/sass-convert" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\Ruby23-x64\bin\ruby.exe" "%~dpn0" %*
